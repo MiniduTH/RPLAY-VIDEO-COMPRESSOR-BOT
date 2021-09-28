3be9ed5fb40dd1b58ae433ca2cb109fec8718ac6
Danish <danish@ultroid.tech>
2021-09-28 23:23:27 +0000

    pyLint: auto-fixes

--- bot/config.py
+++ bot/config.py
@@ -21,7 +21,7 @@ try:
     BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
     DEV = 1322549723
     AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
-    OWNER =  [int(i) for i in os.environ.get("OWNER_ID", "").split(" ")]
+    OWNER = [int(i) for i in os.environ.get("OWNER_ID", "").split(" ")]
     FFMPEG = config(
         "FFMPEG",
         default='ffmpeg -i "{}" -preset ultrafast -c:v libx265 -crf 27 -map 0:v -c:a aac -map 0:a -c:s copy -map 0:s? "{}"',
