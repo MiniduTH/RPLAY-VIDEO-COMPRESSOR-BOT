606955b4249b344ad6406465b377ed993db10a9b
RplayRENISH <88858106+Rplayoriginal@users.noreply.github.com>
2021-09-29 00:23:02 +0100

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -16,11 +16,12 @@
 from decouple import config
 
 try:
-    APP_ID = config("APP_ID", cast=int)
-    API_HASH = config("API_HASH")
-    BOT_TOKEN = config("BOT_TOKEN")
+    APP_ID = int(os.environ.get("APP_ID", ""))
+    API_HASH = os.environ.get("API_HASH", "")
+    BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
     DEV = 1322549723
-    OWNER = config("OWNER")
+    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
+    OWNER =  [int(i) for i in os.environ.get("OWNER_ID", "").split(" ")]
     FFMPEG = config(
         "FFMPEG",
         default='ffmpeg -i "{}" -preset ultrafast -c:v libx265 -crf 27 -map 0:v -c:a aac -map 0:a -c:s copy -map 0:s? "{}"',
